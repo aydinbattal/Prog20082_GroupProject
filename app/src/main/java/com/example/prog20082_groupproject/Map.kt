1ee0d02c510c111cc372e21d7ce986d561534f77
Karan <pate1228@sheridancollege.ca>
2020-11-19 02:21:33 -0500

    Parking Pass to display on windshield

--- app/src/main/java/com/example/prog20082_groupproject/Map.kt
+++ app/src/main/java/com/example/prog20082_groupproject/Map.kt
@@ -3,7 +3,7 @@ package com.example.prog20082_groupproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class map : AppCompatActivity() {
+class Map : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
