b9f576e061d42f69431b70e661fd9a0e42228c1b
Aydin Battal <abattal35@gmail.com>
2020-11-19 02:49:17 -0500

    Changed BookingActivity.kt and activity_booking.xml to BookingFragment.kt and fragment_booking.xml

--- app/src/main/java/com/example/prog20082_groupproject/BookingFragment.kt
+++ app/src/main/java/com/example/prog20082_groupproject/BookingFragment.kt
@@ -3,9 +3,9 @@ package com.example.prog20082_groupproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class BookingActivity : AppCompatActivity() {
+class BookingFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_booking)
+        setContentView(R.layout.fragment_booking)
     }
 }
