c4e882163cdc2cdf07cb1c676681e353693e2424
Aydin Battal <abattal35@gmail.com>
2020-11-18 23:19:37 -0500

    Test from new branch

--- app/src/main/java/com/example/prog20082_groupproject/MainActivity.kt
+++ app/src/main/java/com/example/prog20082_groupproject/MainActivity.kt
@@ -8,7 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    //test
-        //adam commit test
+    //test from new branch
     }
 }
