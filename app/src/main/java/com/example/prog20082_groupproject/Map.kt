f3d43503f6c89bee68edd36c7698511eef39398a
Karan <pate1228@sheridancollege.ca>
2020-11-24 00:13:33 -0500

    Live gradient animation background https://codinginflow.com/tutorials/android/animated-gradient-background but with conversions to kotlin by me and other misc adjustments. Removed main activity

--- app/src/main/java/com/example/prog20082_groupproject/Map.kt
+++ app/src/main/java/com/example/prog20082_groupproject/Map.kt
@@ -1,11 +1,20 @@
 package com.example.prog20082_groupproject
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_map.*
 
 class Map : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        val animDrawable = activity_map.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
     }
 }
